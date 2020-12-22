9d3c60b4f3e1b506419ae58d390d9046f4a37379
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-12-22 13:19:24 -0800

    commit

--- english_leaning.py
+++ english_leaning.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from telegram.ext import Updater, MessageHandler, Filters
+from telegram import InputMediaAudio
 from telegram_util import log_on_fail
 import link_extractor
 import time
@@ -11,9 +12,9 @@ import cached_url
 import export_to_telegraph
 
 with open('token') as f:
-	tele = Updater(f.read().strip(), use_context=True)
-debug_group = tele.bot.get_chat(420074357)
-channel_en = tele.bot.get_chat(-1001414226421)
+	bot = Updater(f.read().strip(), use_context=True).bot
+debug_group = bot.get_chat(420074357)
+channel_en = bot.get_chat(-1001414226421)
 
 # with open('telegraph_token') as f:
 # 	export_to_telegraph.token = f.read().strip()
@@ -27,17 +28,22 @@ def getFile(link):
 	for item in soup.find_all('a', class_='download'):
 		if item.get('href', '').endswith('mp3'):
 			return item.get('href', '')
-		
+	
+@log_on_fail(debug_group)
 def run():
 	for link in link_extractor.getLinks(site):
 		if existing.contain(link):
 			continue
-		# result = export_to_telegraph.export(link, force=True, throw_exception=True) 
+		result = export_to_telegraph.export(link, force=True, throw_exception=True) 
 		file = getFile(link)
 		if not file:
 			continue
 		cached_url.get(file, mode='b', force_cache=True)
-
+		group = [InputMediaAudio(open(
+			cached_url.getFilePath(file), 'rb'), caption=result)]
+		bot.send_media_group(channel_en.id, group, timeout = 20*60)
+		existing.add(link)
+		return
 		
 if __name__ == '__main__':
 	run()

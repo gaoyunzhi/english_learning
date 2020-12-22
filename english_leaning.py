e84ce27587516d07d77cd3cf935dd560e24a2a1d
Yunzhi Gao <gaoyunzhi@gmail.com>
2020-12-22 10:37:24 -0800

    commit

--- english_leaning.py
+++ english_leaning.py
@@ -11,11 +11,19 @@ import export_to_telegraph
 with open('token') as f:
 	token = f.read().strip()
 
+# with open('telegraph_token') as f:
+# 	export_to_telegraph.token = f.read().strip()
+
+site = 'https://www.bbc.co.uk/learningenglish/chinese'
+
 existing = plain_db.loadKeyOnlyDB('existing')
 		
 def run():
-	links = link_extractor.getLinks('https://www.bbc.co.uk/learningenglish/chinese')
-	print(links)
+	for link in link_extractor.getLinks(site):
+		result = export_to_telegraph.export(link, force=True, throw_exception=True) 
+		print(result)
+		return
+	
 			
 if __name__ == '__main__':
 	run()
